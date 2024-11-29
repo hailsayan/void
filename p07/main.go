d7166b72662b90d84c91f9bbe9ef18b1ec874f43
Psyon <sayan.h2020@gmail.com>
2024-11-29 18:52:51 +0330

    add prime num detector

---
+++ p07/main.go
@@ -0,0 +1,34 @@
+package main
+
+//34
+
+import (
+	"fmt"
+	"math"
+)
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	var n int
+	fmt.Println("enter a number: ")
+	fmt.Scan(&n)
+
+	if isPrime(n) {
+		fmt.Printf("%d is a prime number\n", n)
+	} else {
+		fmt.Printf("%d is not a prime number\n", n)
+	}
+
+}
