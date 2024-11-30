1bc3aa8b5c13a04667ae1adab51eb1404bab0462
Psyon <sayan.h2020@gmail.com>
2024-11-30 12:55:22 +0330

    add divisors. from 2 to 500

---
+++ p05-divisors/from2to500/main.go
@@ -0,0 +1,15 @@
+package main
+
+// 37
+
+import "fmt"
+
+func main() {
+	for i := 2; i <= 500; i++ {
+		for j := 1; j <= i; j++ {
+			if i%j == 0 {
+				fmt.Printf("divisors of %d is %d\n", i, j)
+			}
+		}
+	}
+}
