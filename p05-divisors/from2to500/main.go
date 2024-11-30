a3e5d713969c06be27b129ecba1c0ed449639c90
Psyon <sayan.h2020@gmail.com>
2024-11-30 13:27:58 +0330

    add the day of the year

--- p05-divisors/from2to500/main.go
+++ p05-divisors/from2to500/main.go
@@ -8,7 +8,7 @@ func main() {
 	for i := 2; i <= 500; i++ {
 		for j := 1; j <= i; j++ {
 			if i%j == 0 {
-				fmt.Printf("divisors of %d is %d\n", i, j)
+				fmt.Printf("divisor of %d is %d\n", i, j)
 			}
 		}
 	}
---
+++ p09-theNthOfMonth/main.go
@@ -0,0 +1,27 @@
+package main
+
+// 37
+
+import "fmt"
+
+func calculator(r, m int) int {
+	if m <= 6 {
+		n := (m-1)*31 + r
+		return n
+	} else if 7 <= m && m < 11 {
+		n := 186 + (m-6)*30 + r
+		return n
+	} else {
+		n := 336 + r
+		return n
+	}
+}
+
+func main() {
+	var r, m int
+	fmt.Println("enter the day (r) of month (m)")
+	fmt.Scan(&r, &m)
+
+	fmt.Printf("we are on the %dth of the year", calculator(r, m))
+
+}
